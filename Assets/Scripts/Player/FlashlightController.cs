8b46068bf2c20bac4d21fa51a4c5b45afe1763be
Like7 <like7coolmen@gmail.com>
2022-04-09 22:31:43 +0400

    Flashlight Functionality & Created ObjectiveSystem

---
+++ Assets/Scripts/Player/FlashlightController.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashlightController : MonoBehaviour
+{
+
+    [Range(0, 100)]float batteryCharge;
+    [SerializeField]float degredationRate = .01f;
+
+    public Gradient uiColorRange;
+    public Color uiColor;
+    public Renderer ui;
+
+    bool toggled;
+    public Light light;
+    public GameObject[] lightObjects;
+    
+    public PlayerMovement playerMovement;
+    [Range(0, 50)]public float flashBangCost;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        batteryCharge = 100;
+        toggled = false;
+        ToggleLight(toggled);
+
+        ui.material.EnableKeyword("_EMISSION");
+        updateUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool doubleTap = false;
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (toggled && !doubleTap)
+                toggled = false;
+            else
+                toggled = true;
+            ToggleLight(toggled);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            FlashBang();
+        }
+
+        if (toggled)
+        {
+            batteryCharge -= degredationRate * Time.deltaTime;
+            updateUI();
+        }
+
+        if (batteryCharge <= 0)
+        {
+            if (toggled)
+            {
+                toggled = false;
+                ToggleLight(toggled);
+            }
+        }
+    }
+
+    void ToggleLight(bool state)
+    {
+        light.enabled = state;
+        for (int i = 0; i < lightObjects.Length; i++)
+            lightObjects[i].SetActive(state);        
+    }
+
+    void FlashBang()
+    {
+        if ((batteryCharge - flashBangCost) > 0)
+        {
+            toggled = true;
+            ToggleLight(toggled);            
+            playerMovement.FlashBang();
+            batteryCharge -= flashBangCost;
+            updateUI();
+        }        
+    }
+
+    public void AddCharge(float charge)
+    {
+        if ((batteryCharge + charge) < 100)
+        {
+            batteryCharge += charge;
+        }
+        else
+        {
+            batteryCharge = 100;
+        }
+    }
+
+    void updateUI()
+    {
+        uiColor = uiColorRange.Evaluate(batteryCharge/100);
+        //ui.material.color = uiColor;
+        if (toggled)
+            ui.material.SetColor("_EmissionColor", uiColor * 1.5f);
+        else
+        {
+            ui.material.color = Color.black;
+            ui.material.SetColor("_EmissionColor", Color.black * 1.5f);
+        }            
+    }
+}
